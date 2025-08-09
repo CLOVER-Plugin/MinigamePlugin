f1d4f9603607ac76a0ea0e9f1b59a9ff8a479a77
YD <ydking0911@gmail.com>
2025-08-09 22:28:37 +0900

    [FIX] 1라운드/2라운드 수정
    - SoccerBallCommand 콘솔 지원
    - 블럭파괴이벤트 아이템 .clone()으로 지급

--- src/main/java/yd/kingdom/main/listener/BlockBreakDropListener.java
+++ src/main/java/yd/kingdom/main/listener/BlockBreakDropListener.java
@@ -61,29 +61,33 @@ public class BlockBreakDropListener implements Listener {
             reward = new ItemStack(Material.ARROW, 1);
             rewardName = "§7화살";
         } else if (roll <= 45) {
-            reward = itemManager.getItemByKey("실명권");
+            reward = itemManager.getItemByKey("실명권").clone();
             rewardName = "§a그림팀 실명권";
         } else if (roll == 46) {
-            reward = itemManager.getItemByKey("초기화권");
+            reward = itemManager.getItemByKey("초기화권").clone();
             rewardName = "§a그림판 초기화권";
         } else if (roll <= 56) {
-            reward = itemManager.getItemByKey("깽판권");
+            reward = itemManager.getItemByKey("깽판권").clone();
             rewardName = "§b그림판 깽판권";
         } else if (roll <= 86) {
-            reward = itemManager.getItemByKey("점프부스트");
+            reward = itemManager.getItemByKey("점프부스트").clone();
             rewardName = "§c점프력 100배";
         } else if (roll <= 96) {
-            reward = itemManager.getItemByKey("좀비소환권");
+            reward = itemManager.getItemByKey("좀비소환권").clone();
             rewardName = "§d좀비 10마리 소환권";
         } else { // 97~100
-            reward = itemManager.getItemByKey("감옥권");
+            reward = itemManager.getItemByKey("감옥권").clone();
             rewardName = "§e랜덤 감옥권";
         }
 
         if (reward != null) {
             Map<Integer, ItemStack> leftover = p.getInventory().addItem(reward);
             if (!leftover.isEmpty()) {
-                block.getWorld().dropItemNaturally(block.getLocation(), leftover.values().iterator().next());
+                ItemStack drop = leftover.values().iterator().next();
+                // 블록 복원(2틱) 이후, 플레이어 위치에 드롭
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                    p.getWorld().dropItemNaturally(p.getLocation(), drop);
+                }, 3L);
             }
             MessageUtil.send(p, "§a보상 획득: " + rewardName + " §7x1");
         }
